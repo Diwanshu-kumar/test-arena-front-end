110825c20f1808a5936d269e76c85d456771c1b5
Diwanshu <kumardiwanshu00@gmail.com>
2024-08-02 17:50:49 +0530

    feat : code running on sample and submission is completed.

--- Problems/solveProblem.js
+++ Problems/solveProblem.js
@@ -63,3 +63,125 @@ function fetchProblemDetails(problemId) {
 
 // Assuming you have a way to get the problemId, for example:
 fetchProblemDetails(problemId);
+
+document.addEventListener('DOMContentLoaded', () => {
+    // Load saved data from localStorage
+    editor.setSize(null,"500px");
+    const savedCode = localStorage.getItem('codeEditorContent');
+    if (savedCode) {
+        editor.setValue(savedCode);
+    }
+
+    // Save data to localStorage on change
+    editor.on('change', () => {
+        localStorage.setItem('codeEditorContent', editor.getValue());
+    });
+});
+
+const runOutputContainer = document.getElementById("run-output-container");
+let verdict =document.getElementById("verdictOutput");
+
+const runButton = document.getElementById('runButton');
+const submitButton = document.getElementById('submitButton');
+
+// Handle "Run" button click
+runButton.addEventListener('click', () => {
+    runButton.disabled = true;
+    removeRunStatus();
+    const code = editor.getValue();
+    const userId = '1'; // Replace with actual user ID
+    const language = document.getElementById('language').value; // Replace with actual selected language
+    const requestData = {
+        userId: userId,
+        language: language,
+        code: code,
+        problemId: problemId
+    };
+
+    fetch('http://localhost:8080/api/v1/user/run', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(requestData),
+    })
+        .then(response => response.json())
+        .then(data => {
+            runButton.disabled = false;
+            console.log('Success:', data);
+            // Handle the response data here, e.g., display the output
+            document.getElementById("sampleOutput").innerHTML=`${data.result} \n*****************************************\nexecution time = ${data.executionTime} ms`
+        })
+        .catch((error) => {
+            runButton.disabled = false;
+            console.error('Error:', error);
+            const runOutputContainer = document.getElementById("run-output-container");
+            let verdict =document.getElementById("verdictOutput");
+            verdict.innerText = "connection issue";
+            verdict.className = "status error";
+            runOutputContainer.insertBefore(verdict,runOutputContainer.firstChild);
+            // Handle errors here
+        });
+});
+
+const removeRunStatus = () =>{
+    verdict.innerText = "";
+    verdict.className = "";
+}
+
+
+// Handle "submit" button click
+submitButton.addEventListener('click', () => {
+    removeRunStatus();
+    submitButton.disabled = true;
+    const code = editor.getValue();
+    const userId = '1'; // Replace with actual user ID
+    const language = document.getElementById('language').value; // Replace with actual selected language
+    const requestData = {
+        userId: userId,
+        language: language,
+        code: code,
+        problemId: problemId
+    };
+
+    fetch('http://localhost:8080/api/v1/user/submit', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(requestData),
+    })
+        .then(response => response.json())
+        .then(data => {
+            submitButton.disabled = false;
+            console.log('Success:', data);
+            // Handle the response data here, e.g., display the output
+            const sampleOutput =document.getElementById("sampleOutput");
+            if(data.verdict==="AC"){
+                verdict.innerText = "Accepted";
+                verdict.className = "status accepted";
+            }else if(data.verdict==="WA"){
+                verdict.innerText = "Wrong-answer";
+                verdict.className = "status wrong-answer";
+            }else if(data.verdict==="TLE"){
+                verdict.innerText = "TLE";
+                verdict.className = "status tle";
+            }else{
+                verdict.innerText = "error";
+                verdict.className = "status error";
+            }
+            sampleOutput.innerHTML=`${data.result} \n*****************************************\nexecution time = ${data.executionTime} ms`
+            runOutputContainer.insertBefore(verdict,runOutputContainer.firstChild);
+        })
+        .catch((error) => {
+            submitButton.disabled = false;
+            console.error('Error:', error);
+            const runOutputContainer = document.getElementById("run-output-container");
+            let verdict =document.getElementById("verdictOutput");
+            verdict.innerText = "connection issue";
+            verdict.className = "status error";
+            runOutputContainer.insertBefore(verdict,runOutputContainer.firstChild);
+            // Handle errors here
+        });
+});
+
