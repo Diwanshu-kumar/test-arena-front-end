fb0e2556c46eb7a87a2fb4e767d4b89ed9abe09e
Diwanshu <kumardiwanshu00@gmail.com>
2024-08-04 19:21:21 +0530

    fix : deleted unused files and import

--- config.js
+++ config.js
@@ -1,8 +1,4 @@
 // Define a global configuration object
-window.AppConfig = {
-    API_BASE_URL: 'http://localhost:8080/api/v1'
-};
-
 const CONFIG = {
     API_BASE_URL: 'http://localhost:8080/api/v1',
 };
